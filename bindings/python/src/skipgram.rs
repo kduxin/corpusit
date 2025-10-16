f75f899ffd2a4046f1851c2194bca1dc7617cd2e
duxin <duxin_be@outlook.com>
2025-10-16 15:47:29 +0900

    v0.2.0: removed Vocab struct

--- bindings/python/src/skipgram.rs
+++ bindings/python/src/skipgram.rs
@@ -1,212 +1,271 @@
-use corpusit::SGDatasetConfig;
 use pyo3::prelude::*;
-use std::sync::Arc;
+use std::collections::HashMap;
 extern crate corpusit as cit;
-use crate::vocab::PyVocab;
-use cit::fileread::FileIterateMode;
-use cit::skipgram::{SGDataset, SGDatasetBuilder, SGDatasetIter, SGDatasetPosIter};
+use cit::skipgram::{SGConfig, SGPosIter, SGIter, SGConfigWithTokenization, SGIterWithTokenization};
 use ndarray::{Array1, Array2, Axis};
 use numpy::{PyArray1, PyArray2};
 
-#[pyclass(module = "corpusit", name = "SkipGramDataset", unsendable)]
-pub struct PySGDataset {
-    dataset: SGDataset,
+
+// ----------------------- SkipGram Python Bindings ------------------------
+
+#[pyclass(module = "corpusit", name = "SkipGramConfig", unsendable)]
+pub struct PySGConfig {
+    config: SGConfig,
 }
 
-impl From<PySGDataset> for SGDataset {
-    fn from(dataset: PySGDataset) -> Self {
-        dataset.dataset
+impl From<PySGConfig> for SGConfig {
+    fn from(config: PySGConfig) -> Self {
+        config.config
     }
 }
 
-impl From<SGDataset> for PySGDataset {
-    fn from(dataset: SGDataset) -> Self {
-        Self { dataset: dataset }
+impl From<SGConfig> for PySGConfig {
+    fn from(config: SGConfig) -> Self {
+        Self { config: config }
     }
 }
 
 #[pymethods]
-impl PySGDataset {
-    #[pyo3(signature = (path_to_corpus, vocab, win_size = 10, sep = " ", mode = "shuffle", subsample = 1e-5, power = 0.75, n_neg = 1))]
+impl PySGConfig {
+    #[pyo3(signature = (word_counts, win_size = 5, subsample = 1e-3, power = 0.75, n_neg = 1))]
     #[new]
     fn new(
-        path_to_corpus: &str,
-        vocab: &PyVocab,
+        word_counts: HashMap<usize, u64>,
         win_size: usize,
-        sep: &str,
-        mode: &str,
         subsample: f64,
         power: f64,
         n_neg: usize,
     ) -> Self {
-        let config = SGDatasetConfig {
-            corpus_path: path_to_corpus.to_string(),
-            win_size: win_size,
-            sep: sep.to_string(),
-            mode: match mode {
-                "shuffle" => FileIterateMode::SHUFFLE,
-                "onepass" => FileIterateMode::ONEPASS,
-                "repeat" => FileIterateMode::REPEAT,
-                _other => panic!("Unknown file iterating mode {}", mode),
-            },
-            subsample: subsample,
-            power: power,
-            n_neg: n_neg,
-        };
-        let dataset = SGDatasetBuilder::new(config).build_with_vocab(&vocab.vocab.read().unwrap());
-        Self { dataset: dataset }
+        let config = SGConfig::new(word_counts)
+            .with_win_size(win_size)
+            .with_subsample(subsample)
+            .with_power(power)
+            .with_n_neg(n_neg);
+        
+        Self { config: config }
     }
 
-    #[pyo3(signature = (batch_size, seed = 0, num_threads = 4))]
+
+    #[pyo3(signature = (seed = 0))]
     pub fn positive_sampler(
         &self,
-        batch_size: usize,
         seed: u64,
-        num_threads: usize,
-    ) -> PySGDatasetPosIter {
-        PySGDatasetPosIter {
-            it: self.dataset.positive_sampler(seed, num_threads),
-            batch_size: batch_size,
+    ) -> PySGPosIter {
+        PySGPosIter {
+            it: SGPosIter::new(self.config.clone(), seed),
         }
     }
 
-    #[pyo3(signature = (batch_size, seed = 0, num_threads = 4))]
+    #[pyo3(signature = (seed = 0, num_threads = 4))]
     pub fn sampler(
         &self,
-        batch_size: usize,
         seed: u64,
         num_threads: usize,
-    ) -> PySGDatasetIter {
-        PySGDatasetIter {
-            it: self.dataset.sampler(seed, num_threads),
-            batch_size: batch_size,
+    ) -> PySGIter {
+        PySGIter {
+            it: SGIter::new(self.config.clone(), seed, num_threads),
         }
-    }
-
-    #[getter]
-    pub fn vocab(&self) -> PyVocab {
-        PyVocab::from(Arc::clone(&self.dataset.vocab))
     }
 }
 
 #[pyclass(module = "corpusit", name = "SkipGramPosIter", unsendable)]
-pub struct PySGDatasetPosIter {
-    it: SGDatasetPosIter,
-    batch_size: usize,
+pub struct PySGPosIter {
+    it: SGPosIter,
 }
 
 #[pymethods]
-impl PySGDatasetPosIter {
-    pub fn __iter__(slf: PyRef<Self>) -> PyRef<Self> {
-        slf
+impl PySGPosIter {
+    pub fn process_sequence(&mut self, sequence: Vec<usize>) -> Py<PyArray2<i64>> {
+        let pairs = self.it.process_sequence(&sequence);
+        let mut arr = Array2::<i64>::default((pairs.len(), 2));
+        for (i, mut row) in arr.axis_iter_mut(Axis(0)).enumerate() {
+            let pair = pairs[i];
+            row[0] = pair[0] as i64;
+            row[1] = pair[1] as i64;
+        }
+
+        Python::with_gil(|py| {
+            let y = PyArray2::from_owned_array_bound(py, arr);
+            y.to_owned().into()
+        })
     }
 
-    pub fn __next__(mut slf: PyRefMut<Self>) -> Option<Py<PyArray2<i64>>> {
-        let batch_size = slf.batch_size;
-        let batch = slf.it.next_batch(batch_size);
-        match batch {
-            Some(batch) => {
-                let mut arr = Array2::<i64>::default((batch.len(), 2));
-                for (i, mut row) in arr.axis_iter_mut(Axis(0)).enumerate() {
-                    let pair = batch[i];
-                    row[0] = pair[0] as i64;
-                    row[1] = pair[1] as i64;
-                }
-
-                let res = Python::with_gil(|py| {
-                    let y = PyArray2::from_owned_array_bound(py, arr);
-                    y.to_owned().into()
-                });
-                Some(res)
-            }
-            None => None,
+    pub fn process_sequences(&mut self, sequences: Vec<Vec<usize>>) -> Py<PyArray2<i64>> {
+        let pairs = self.it.process_sequences(&sequences);
+        let mut arr = Array2::<i64>::default((pairs.len(), 2));
+        for (i, mut row) in arr.axis_iter_mut(Axis(0)).enumerate() {
+            let pair = pairs[i];
+            row[0] = pair[0] as i64;
+            row[1] = pair[1] as i64;
         }
+
+        Python::with_gil(|py| {
+            let y = PyArray2::from_owned_array_bound(py, arr);
+            y.to_owned().into()
+        })
     }
 }
 
 #[pyclass(module = "corpusit", name = "SkipGramIter", unsendable)]
-pub struct PySGDatasetIter {
-    it: SGDatasetIter,
-    batch_size: usize,
+pub struct PySGIter {
+    it: SGIter,
 }
 
 #[pymethods]
-impl PySGDatasetIter {
-    pub fn __iter__(slf: PyRef<Self>) -> PyRef<Self> {
-        slf
+impl PySGIter {
+    pub fn process_sequence(&mut self, sequence: Vec<usize>) -> (Py<PyArray2<i64>>, Py<PyArray1<bool>>) {
+        let (pairs, labels) = self.it.process_sequence(&sequence);
+        let mut pairs_arr = Array2::<i64>::default((pairs.len(), 2));
+        let mut labels_arr = Array1::<bool>::default(labels.len());
+        
+        for (i, mut row) in pairs_arr.axis_iter_mut(Axis(0)).enumerate() {
+            let pair = pairs[i];
+            row[0] = pair[0] as i64;
+            row[1] = pair[1] as i64;
+        }
+        
+        for (i, val) in labels_arr.iter_mut().enumerate() {
+            *val = labels[i];
+        }
+
+        Python::with_gil(|py| {
+            (
+                PyArray2::from_owned_array_bound(py, pairs_arr).to_owned().into(),
+                PyArray1::from_owned_array_bound(py, labels_arr).to_owned().into(),
+            )
+        })
     }
 
-    pub fn __next__(mut slf: PyRefMut<Self>) -> Option<(Py<PyArray2<i64>>, Py<PyArray1<bool>>)> {
-        let batch_size = slf.batch_size;
-        let batch = slf.it.next_batch(batch_size);
-        match batch {
-            Some(batch) => {
-                let len = batch.0.len();
-                let mut pairs = Array2::<i64>::default((len, 2));
-                let mut labels = Array1::<bool>::default(len);
-                for (i, mut row) in pairs.axis_iter_mut(Axis(0)).enumerate() {
-                    let pair = batch.0[i];
-                    row[0] = pair[0] as i64;
-                    row[1] = pair[1] as i64;
-                }
-                for (i, val) in labels.iter_mut().enumerate() {
-                    *val = batch.1[i];
-                }
+    pub fn process_sequences(&mut self, sequences: Vec<Vec<usize>>) -> (Py<PyArray2<i64>>, Py<PyArray1<bool>>) {
+        let (pairs, labels) = self.it.process_sequences(&sequences);
+        let mut pairs_arr = Array2::<i64>::default((pairs.len(), 2));
+        let mut labels_arr = Array1::<bool>::default(labels.len());
+        
+        for (i, mut row) in pairs_arr.axis_iter_mut(Axis(0)).enumerate() {
+            let pair = pairs[i];
+            row[0] = pair[0] as i64;
+            row[1] = pair[1] as i64;
+        }
+        
+        for (i, val) in labels_arr.iter_mut().enumerate() {
+            *val = labels[i];
+        }
 
-                Python::with_gil(|py| {
-                    Some((
-                        PyArray2::from_owned_array_bound(py, pairs).to_owned().into(),
-                        PyArray1::from_owned_array_bound(py, labels).to_owned().into(),
-                    ))
-                })
-            }
-            None => None,
+        Python::with_gil(|py| {
+            (
+                PyArray2::from_owned_array_bound(py, pairs_arr).to_owned().into(),
+                PyArray1::from_owned_array_bound(py, labels_arr).to_owned().into(),
+            )
+        })
+    }
+
+}
+
+// ----------------------- SkipGram with Tokenization Python Bindings ------------------------
+
+#[pyclass(module = "corpusit", name = "SkipGramConfigWithTokenization", unsendable)]
+pub struct PySGConfigWithTokenization {
+    config: SGConfigWithTokenization,
+}
+
+impl From<PySGConfigWithTokenization> for SGConfigWithTokenization {
+    fn from(config: PySGConfigWithTokenization) -> Self {
+        config.config
+    }
+}
+
+impl From<SGConfigWithTokenization> for PySGConfigWithTokenization {
+    fn from(config: SGConfigWithTokenization) -> Self {
+        Self { config: config }
+    }
+}
+
+#[pymethods]
+impl PySGConfigWithTokenization {
+    #[pyo3(signature = (word_counts, word_to_id, separator = " ", win_size = 5, subsample = 1e-3, power = 0.75, n_neg = 1))]
+    #[new]
+    fn new(
+        word_counts: HashMap<usize, u64>,
+        word_to_id: HashMap<String, usize>,
+        separator: &str,
+        win_size: usize,
+        subsample: f64,
+        power: f64,
+        n_neg: usize,
+    ) -> Self {
+        let config = SGConfigWithTokenization::new(word_counts, word_to_id, separator.to_string())
+            .with_win_size(win_size)
+            .with_subsample(subsample)
+            .with_power(power)
+            .with_n_neg(n_neg);
+        
+        Self { config: config }
+    }
+
+    #[pyo3(signature = (seed = 0, num_threads = 4))]
+    pub fn sampler(
+        &self,
+        seed: u64,
+        num_threads: usize,
+    ) -> PySGIterWithTokenization {
+        PySGIterWithTokenization {
+            it: SGIterWithTokenization::new(self.config.clone(), seed, num_threads),
         }
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use crate::skipgram::PySGDataset;
-    use crate::vocab::PyVocab;
-    use tqdm::Iter;
+#[pyclass(module = "corpusit", name = "SkipGramIterWithTokenization", unsendable)]
+pub struct PySGIterWithTokenization {
+    it: SGIterWithTokenization,
+}
 
-    #[test]
-    fn iterate_positive() {
-        let path =
-            "/home/duxin/code/firelang/data/wacky/wacky_combined.txt.UNCASED.tokens".to_string();
-        // let path = "/home/duxin/code/firelang/data/wacky/wacky_combined.txt.1000000".to_string();
-        let vocab_path = path.clone() + ".vocab.bin";
-
-        let vocab = PyVocab::from_bin(vocab_path, Some(100), Some(99999999), Some("<unk>"));
-        let dataset = PySGDataset::new(&path, &vocab, 10, " ", "shuffle", 1e-5, 0.75, 1);
-        let mut it = dataset.positive_sampler(30000, 0, 4);
-        for _ in (0..1000).into_iter().tqdm() {
-            let x = it.it.next_batch(30000);
-            if let Some(x) = x {
-                if x.len() >= 60000 {
-                    println!("123");
-                }
-            }
+#[pymethods]
+impl PySGIterWithTokenization {
+    /// Process string sequences with tokenization
+    pub fn process_string_sequences(&mut self, text_sequences: Vec<String>) -> (Py<PyArray2<i64>>, Py<PyArray1<bool>>) {
+        let (pairs, labels) = self.it.process_string_sequences(&text_sequences);
+        let mut pairs_arr = Array2::<i64>::default((pairs.len(), 2));
+        let mut labels_arr = Array1::<bool>::default(labels.len());
+        
+        for (i, mut row) in pairs_arr.axis_iter_mut(Axis(0)).enumerate() {
+            let pair = pairs[i];
+            row[0] = pair[0] as i64;
+            row[1] = pair[1] as i64;
         }
+        
+        for (i, val) in labels_arr.iter_mut().enumerate() {
+            *val = labels[i];
+        }
+
+        Python::with_gil(|py| {
+            (
+                PyArray2::from_owned_array_bound(py, pairs_arr).to_owned().into(),
+                PyArray1::from_owned_array_bound(py, labels_arr).to_owned().into(),
+            )
+        })
     }
 
-    #[test]
-    fn iterate() {
-        let path =
-            "/home/duxin/code/firelang/data/wacky/wacky_combined.txt.UNCASED.tokens".to_string();
-        let vocab_path = path.clone() + ".vocab.bin";
-        let vocab = PyVocab::from_bin(vocab_path, Some(100), Some(99999999), Some("<unk>"));
-        let dataset = PySGDataset::new(&path, &vocab, 10, " ", "shuffle", 1e-5, 0.75, 1);
-
-        let mut it = dataset.sampler(30000, 0, 4);
-        for _ in (0..1000).into_iter().tqdm() {
-            let x = it.it.next_batch(30000);
-            if let Some(x) = x {
-                if x.1.len() >= 1000000000 {
-                    println!("123");
-                }
-            }
+    /// Process a single string with tokenization
+    pub fn process_string(&mut self, text: String) -> (Py<PyArray2<i64>>, Py<PyArray1<bool>>) {
+        let (pairs, labels) = self.it.process_string(&text);
+        let mut pairs_arr = Array2::<i64>::default((pairs.len(), 2));
+        let mut labels_arr = Array1::<bool>::default(labels.len());
+        
+        for (i, mut row) in pairs_arr.axis_iter_mut(Axis(0)).enumerate() {
+            let pair = pairs[i];
+            row[0] = pair[0] as i64;
+            row[1] = pair[1] as i64;
         }
+        
+        for (i, val) in labels_arr.iter_mut().enumerate() {
+            *val = labels[i];
+        }
+
+        Python::with_gil(|py| {
+            (
+                PyArray2::from_owned_array_bound(py, pairs_arr).to_owned().into(),
+                PyArray1::from_owned_array_bound(py, labels_arr).to_owned().into(),
+            )
+        })
     }
 }
